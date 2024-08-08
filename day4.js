57f2a4dc9f4c4aac470080fc780e8b6795cfddfd
jmk11223344 <ppprrr080103@gmail.com>
2024-08-08 12:00:08 +0900

    Add files via upload

---
+++ day4.js
@@ -0,0 +1,76 @@
+
+/*let score = 100;
+
+score = 100;
+
+
+switch(score){
+    case  95  : 
+        console.log(`A`);
+        break;
+    case   100  : 
+        console.log(`B`);
+        break;
+    case  80  : 
+        console.log(`C`);
+        break;
+    default:
+        console.log(`F`);
+    break
+}
+*/
+
+/*
+let i = 1;
+while(i < 10/2){
+    console.log (i*2);
+    i = i+1
+}
+*/
+
+/*
+do{
+    console.log(i1);
+    i1 + i1 + 1;
+}while(i1 < 0);
+*/
+
+
+
+/*
+
+for(let j =1; j<10; j = j+1){
+    console.log(2,'x',j,'=',j*2);
+}
+
+*/
+
+
+
+
+/*
+i=1;
+while(i<10){
+    console.log(2,`X`,i,`=`,2*i);
+    i= i+1
+}
+
+
+*/
+
+/*
+let food = [`apple`, `banana`, `orange`];
+
+for(fd in food){
+    console.log(food[fd]);
+}
+
+let rank = { '국어' : 5,'수학':3,'사회':2,'과학':1};
+
+for(key in rank){
+    console.log(key,':',rank[key]);
+}
+*/
+
+
+
